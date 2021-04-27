43c2ebfe74b5f3d2a0fa2fab8ec457ca935d965b
Haroon Rafique <haroon.rafique@stfc.ac.uk>
2021-04-27 11:37:39 +0100

    <sim>[00: PSB] AFS sims added for run on HPC-Batch

---
+++ 00_NewPTC_Test/PSB/newPTC/CERN_PSB_spacecharge_AFS/git_adds.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo 'git_adds started in'
+pwd
+
+git add *.sh
+git add *.py
+git add *.out
+git add Output/output.dat.mat
+git add Input/
+git add Tables/
+git add ptc/
+#~ git add Output/
+
+echo 'git_adds completed in'
+pwd
