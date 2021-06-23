f4208b6f8c8c9d82dbd459650817ac98e8004a26
Haroon Rafique <haroon.rafique@stfc.ac.uk>
2021-06-23 11:14:29 +0100

    <add>[00: PS_IB: CERN_oldPTC Ramp only tests] 0_H_07 and 0_H_21 sims added for HPB-Batch run

---
+++ 00_NewPTC_Test/PS_Injection_Bump/CERN_oldPTC_Ramp_Only/0_H_07/PyORBIT/clean_run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pickle and output files
+rm -r output
+rm -r bunch_output
+rm -r lost
+rm -r input
+
+rm *.tfs
+rm *.ptc
+rm *.png
+rm PTC-PyORBIT_flat_file.flt
+rm tunespread.dat
+rm madx.ps
+rm All_Twiss/*
+
+. clean_junk.sh
