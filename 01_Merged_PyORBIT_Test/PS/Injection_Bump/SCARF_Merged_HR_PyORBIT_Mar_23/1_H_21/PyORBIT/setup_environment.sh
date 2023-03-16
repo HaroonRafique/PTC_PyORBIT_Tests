d880efaf1dadc36dbf3ccf0e82b86f55b73c7817
Haroon Rafique <haroon.rafique@stfc.ac.uk>
2023-03-16 13:23:44 +0000

    <add>[01/PS/IB] repeat tests to run on SCARF

---
+++ 01_Merged_PyORBIT_Test/PS/Injection_Bump/SCARF_Merged_HR_PyORBIT_Mar_23/1_H_21/PyORBIT/setup_environment.sh
@@ -0,0 +1,22 @@
+# script to setup the PyOrbit environment 
+# execute like: . setup_environment.sh
+
+pyOrbit_dir=/apps20/contrib/ptc-pyorbit/PTC-PyORBIT/Merged_HR_PyORBIT_2/py-orbit
+
+source ${pyOrbit_dir}/customEnvironment.sh
+echo "customEnvironment done"
+source ${pyOrbit_dir}/../virtualenvs/py2.7/bin/activate
+echo "python packages charged"
+#source ${pyOrbit_dir}/../setup_ifort.sh
+#echo "ifort charged (necessary for running)"
+
+which python
+
+ORBIT_ROOT_fullpath=`readlink -f ${ORBIT_ROOT}` 
+echo 
+echo "*****************************************************"
+echo 
+echo "full PyOrbit path:  ${ORBIT_ROOT_fullpath}"
+echo
+. ${ORBIT_ROOT}/../CheckGitStatus.sh ${ORBIT_ROOT_fullpath}
+
